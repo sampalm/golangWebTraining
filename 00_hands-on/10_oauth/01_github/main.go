5a99c88671f5f408c118915872d8821354b7b0ea
sampalm <samuelpalmeira@outlook.com>
2018-04-29 00:29:46 -0300

    Updates main file to use oauth

--- 00_hands-on/10_oauth/01_github/main.go
+++ 00_hands-on/10_oauth/01_github/main.go
@@ -1,20 +1,17 @@
 package oauthweb
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 
-	"google.golang.org/appengine"
-
 	"github.com/gorilla/sessions"
-	"google.golang.org/appengine/urlfetch"
 
 	"github.com/satori/go.uuid"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/memcache"
 )
 
 var root = "http://localhost:8080"
@@ -37,108 +34,92 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "oauth")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// Generates a new unique ID
 	id, err := uuid.NewV4()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	session.Values["state"] = id.String()
+	// Makes a new cookie_session if not exits
+	session, err := r.Cookie("oauth_session")
+	if err != nil {
+		session = &http.Cookie{
+			Name:     "oauth_session",
+			Value:    id.String(),
+			HttpOnly: true,
+		}
+		http.SetCookie(w, session)
+	}
+	// Get oAuth for Github API
+	auth := NewGitOAuth(fmt.Sprintf("%s/oauth-github", root), id.String())
+	// Save oAuth into a memcache and use session id as a key
+	ctx := appengine.NewContext(r)
+	_, err = memcache.Get(ctx, session.Value)
+	if err != nil {
+		if err := memcache.Gob.Set(ctx, &memcache.Item{
+			Key:    session.Value,
+			Object: auth,
+		}); err != nil {
+			log.Printf("**********************************************************\nloginHandler Error: %v\n", err)
+			return
+		}
+	}
 
-	values := make(url.Values)
-	values.Add("client_id", "88bccd52adcc738aed04")
-	values.Add("redirect_uri", fmt.Sprintf("%s/oauth-github", root))
-	values.Add("scope", "user:email")
-	values.Add("state", id.String())
-
-	session.Save(r, w)
-	http.Redirect(w, r, fmt.Sprintf("https://github.com/login/oauth/authorize?%s", values.Encode()), 303)
-
+	// Get user authentication url
+	authURL, err := auth.GetAuthURI()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	// Redirect user to Github authentication page
+	http.Redirect(w, r, authURL, 303)
 }
 func oauthHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "oauth")
+	// Check if session exists
+	session, err := r.Cookie("oauth_session")
 	if err != nil {
-		log.Println(err)
+		log.Println("oauthHandler Cookie error: " + err.Error())
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	// Gen a variable to receive oauth pointer from memcache
+	var auth *oAuth
+	ctx := appengine.NewContext(r)
+	// Check if memcache has been set
+	_, err = memcache.Gob.Get(ctx, session.Value, &auth)
+	if err != nil {
+		log.Println("oauthHandler MemCache error: " + err.Error())
+		http.Error(w, err.Error(), 500)
 		return
 	}
 	// Parse URL values
 	response, err := url.ParseQuery(r.URL.RawQuery)
 	if err != nil {
-		log.Println(err)
+		log.Println("oauthHandler ParseQuery error: " + err.Error())
 		return
 	}
-	// Check code authenticity
-	if response.Get("state"); session.Values["state"] != response.Get("state") {
-		io.WriteString(w, "Cross site attack detected! \n")
-		log.Printf("Your code: %v, Your sCode: %v", response.Get("state"), session.Values["state"])
+	// Check if authentication url has been changed.
+	if auth.State != response.Get("state") {
+		log.Println(fmt.Errorf("oauthHandler Error: oauth STATE undefined"))
 		return
 	}
-	io.WriteString(w, "Making the authorization... ")
-
-	values := make(url.Values)
-	values.Add("client_id", "88bccd52adcc738aed04")
-	values.Add("client_secret", "7888bc468d37fcc61bbae40ba51647aece853f60")
-	values.Add("code", response.Get("code"))
-	values.Add("state", response.Get("state"))
-
-	token, err := accessToken(r, values)
-	if err != nil {
-		http.Error(w, err.Error(), 501)
-	}
-	email, err := getEmail(r, token)
-	if err != nil {
-		http.Error(w, err.Error(), 501)
+	// Now try to generate a token access
+	auth.Code = response.Get("code")
+	if err := auth.GetAccessToken(ctx); err != nil {
+		log.Println("oauthHandler GetAccessToken error: " + err.Error())
 		return
 	}
-	session.Values["email"] = email
-	session.Save(r, w)
-	io.WriteString(w, "Your email: "+email)
-	// TODO: Make redirect to users page
-}
-func accessToken(r *http.Request, values url.Values) (string, error) {
-	ctx := appengine.NewContext(r)
-	client := urlfetch.Client(ctx)
-	res, err := client.PostForm("https://github.com/login/oauth/access_token", values)
+	// Now we have an api token and we can make requests to the api.
+	// To test we'll get users email
+	emails, err := auth.GetEmails(ctx)
 	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-	bs, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-	response, err := url.ParseQuery(string(bs))
-	if err != nil {
-		return "", err
-	}
-	return response.Get("access_token"), nil
-}
-func getEmail(r *http.Request, token string) (string, error) {
-	var data []struct {
-		Email      string
-		Verified   bool
-		Primary    bool
-		Visibility string
+		log.Println("oauthHandler GetEmails error: " + err.Error())
+		return
 	}
 
-	ctx := appengine.NewContext(r)
-	client := urlfetch.Client(ctx)
-	res, err := client.Get("https://api.github.com/user/emails?access_token=" + token)
-	if err != nil {
-		return "", err
+	if len(emails) == 0 {
+		io.WriteString(w, "email not found")
+		return
 	}
-	defer res.Body.Close()
-
-	err = json.NewDecoder(res.Body).Decode(&data)
-	if err != nil {
-		return "", err
-	}
-	if len(data) == 0 {
-		return "", fmt.Errorf("no data found")
-	}
-	return data[0].Email, nil
+	io.WriteString(w, "Your email: "+emails[0].Email)
 }
