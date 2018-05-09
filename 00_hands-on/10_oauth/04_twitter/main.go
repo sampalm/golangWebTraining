b6698aaf8137f8a6fc412dbd79f11dce19dfda22
sampalm <samuelpalmeira@outlook.com>
2018-05-09 15:34:54 -0300

    Adds a function to get users identity

--- 00_hands-on/10_oauth/04_twitter/main.go
+++ 00_hands-on/10_oauth/04_twitter/main.go
@@ -35,6 +35,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
 	// Generates a new unique ID
 	id, err := uuid.NewV4()
 	if err != nil {
@@ -53,19 +54,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a twitter aouth
 	redirectURI := fmt.Sprintf("%s/oauth", root)
 	oAuth := NewTwitterOAuth(redirectURI, id.String()) // Change to NewGitOAuth() to use Github api
-	// Create a memcache to save this session
-	ctx := appengine.NewContext(r)
-	_, err = memcache.Get(ctx, session.Value)
-	if err != nil {
-		if err := memcache.Gob.Set(ctx, &memcache.Item{
-			Key:    session.Value,
-			Object: &oAuth,
-		}); err != nil {
-			log.Printf("loginHandler memCache Error: %v", err)
-			http.Error(w, "Something went wront, try again later.", 500)
-			return
-		}
-	}
 	// Generate authorization URL
 	authURL, err := oAuth.GetAuthURI(ctx)
 	if err != nil {
@@ -73,23 +61,35 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+	// Create a memcache to save this session
+	_, err = memcache.Get(ctx, session.Value)
+	if err != nil {
+		if err := memcache.Gob.Set(ctx, &memcache.Item{
+			Key:    session.Value,
+			Object: oAuth,
+		}); err != nil {
+			log.Printf("loginHandler memCache Error: %v", err)
+			http.Error(w, "Something went wront, try again later.", 500)
+			return
+		}
+	}
 	// Redirect the user to the dropbox authenticate page
-	http.Redirect(w, r, authURL, 303)
+	http.Redirect(w, r, authURL, 302)
 }
 func oauthHandler(w http.ResponseWriter, r *http.Request) {
 	// Check session state
 	session, err := r.Cookie("oauth")
 	if err != nil {
-		http.Redirect(w, r, "/", 303)
+		http.Redirect(w, r, "/", 302)
 		return
 	}
 	// Checks for authentication
 	values, err := url.ParseQuery(r.URL.RawQuery)
 	if err != nil {
-		log.Println(fmt.Errorf("oauthHandler Error: oauth STATE undefined"))
+		log.Println(fmt.Errorf("oauthHandler Error: failed to get HTTP response"))
 		session.MaxAge = -1
 		http.SetCookie(w, session)
-		http.Redirect(w, r, "/", 303)
+		http.Redirect(w, r, "/", 302)
 		return
 	}
 	// Get oauth from memcache
@@ -101,5 +101,34 @@ func oauthHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong, try again later.", 500)
 		return
 	}
-	log.Printf("values found: %v\n", values)
+	// Set oauth_verifier to get an access_token
+	oauth.Verifier = values.Get("oauth_verifier")
+	log.Printf("***** LOG REDIRECTING THE USER WITH SUCCESS: OTK=%s, NTK=%s, VERIFIER=%s\n", oauth.OTokenID, values.Get("oauth_token"), oauth.Verifier)
+	if err := oauth.GetAccessToken(ctx); err != nil {
+		log.Printf("oauthHandler Error: %v", err)
+		http.Error(w, "Something went wrong, try again later.", 500)
+		return
+	}
+	log.Println("READY TO REQUEST USER IDENTITY.")
+	// Get user identity
+	user, err := oauth.GetUser(ctx)
+	if err != nil {
+		log.Printf("oauthHandler Error: %v", err)
+		http.Error(w, "Something went wrong, try again later.", 500)
+		return
+	}
+
+	io.WriteString(w, `<!DOCTYPE html>
+		<html>
+		<head><title>Github oAuth</title></head>
+		<body>
+		<img src="`+user.Avatar+`" width="150px">
+		<p>`+user.Name+`</p>
+		<p>Email: `+user.Email+`</p>
+		<ol>
+			<li>ID: `+user.ID+`</li>
+		</ol>
+		</body>
+		</html>
+	`)
 }
