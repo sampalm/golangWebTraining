a7d4ef7067ba309389fcf0852df184a39b6c0be4
sampalm <samuelpalmeira@outlook.com>
2018-05-11 10:37:43 -0300

    Modifies encode_signature and fixes return of encode_newheader

--- 00_hands-on/10_oauth/04_twitter/util.go
+++ 00_hands-on/10_oauth/04_twitter/util.go
@@ -17,7 +17,7 @@ func EncodeParams(s string) string {
 }
 
 // EncodeSignature encodes the request header and returns a oauth_signature.
-func EncodeSignature(method string, requestURI string, auth *oAuthV4) (string, url.Values) {
+func EncodeSignature(method string, requestURI string, auth *oAuthV4, params map[string]string) (string, url.Values) {
 	t := time.Now().Unix()
 	values := make(url.Values)
 	values.Add("oauth_consumer_key", auth.ClientID)
@@ -26,25 +26,32 @@ func EncodeSignature(method string, requestURI string, auth *oAuthV4) (string, u
 	values.Add("oauth_timestamp", fmt.Sprintf("%v", t))
 	values.Add("oauth_token", auth.OTokenID)
 	values.Add("oauth_version", "1.0")
-	params := strings.Replace(values.Encode(), "&", "%26", -1)
-	params = strings.Replace(params, "=", "%3D", -1)
-	params = fmt.Sprintf("%s&%s&%s", method, EncodeParams(requestURI), strings.Replace(params, "+", "%20", -1))
-	log.Printf("EncodeSignature BODY: %s\n", params)
+	if params != nil {
+		for where, key := range params {
+			values.Add(where, key)
+		}
+	}
+
+	encode := strings.Replace(values.Encode(), "&", "%26", -1)
+	encode = strings.Replace(encode, "=", "%3D", -1)
+	encode = fmt.Sprintf("%s&%s&%s", method, EncodeParams(requestURI), strings.Replace(encode, "+", "%20", -1))
+	log.Printf("EncodeSignature BODY: %s\n", encode)
 	// GET SIGNIN KEY
 	signKEY := fmt.Sprintf("%s&%s", EncodeParams(auth.SecretID), EncodeParams(auth.OTokenSecretID))
 	// GENERATE HASH
 	mac := hmac.New(sha1.New, []byte(signKEY))
-	mac.Write([]byte(params))
+	mac.Write([]byte(encode))
 	signature := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+	values.Add("oauth_signature", signature)
 	return signature, values
 }
 
 // EncodeNewHeader returns a new encoded header to authorization requests.
 func EncodeNewHeader(signature string, values url.Values) string {
-	return fmt.Sprintf("OAuth oauth_consumer_key=%s, oauth_nonce=%s, oauth_signature=%s, oauth_signature_method=%s, oauth_timestamp=%s, oauth_token=%s, oauth_version=%s", values.Get("oauth_consumer_key"), values.Get("oauth_nonce"), EncodeParams(signature), values.Get("oauth_signature_method"), values.Get("oauth_timestamp"), values.Get("oauth_token"), values.Get("oauth_version"))
+	return fmt.Sprintf("OAuth %s", strings.Replace(values.Encode(), "&", ", ", -1))
 }
 
 // EncodeNewHeaderHTTP returns a new encoded HTTP header to authorization requests.
 func EncodeNewHeaderHTTP(requestURI string, signature string, values url.Values) string {
-	return fmt.Sprintf("%s?oauth_consumer_key=%s&oauth_nonce=%s&oauth_signature=%s&oauth_signature_method=%s&oauth_timestamp=%s&oauth_token=%s&oauth_version=%s", requestURI, values.Get("oauth_consumer_key"), values.Get("oauth_nonce"), EncodeParams(signature), values.Get("oauth_signature_method"), values.Get("oauth_timestamp"), values.Get("oauth_token"), values.Get("oauth_version"))
+	return fmt.Sprintf("%s?%s", requestURI, values.Encode())
 }
